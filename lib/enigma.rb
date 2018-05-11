34b7c6c7a085e5dd1521cd96cab1157ad04bb966
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-05-11 12:29:02 -0600

    Created enigma and test files. Created first test for key generation.

---
+++ lib/enigma.rb
@@ -0,0 +1,17 @@
+require 'pry'
+
+class Enigma
+  def initialize
+
+  end
+
+def generate_key
+  key =
+  number_array = [0..9]
+  5.times do
+    key << numer_array.sample.to_s
+  end
+end
+
+
+end
---
+++ test/enigma_test.rb
@@ -0,0 +1,20 @@
+require 'pry'
+
+gem 'minitest', '~> 5.2'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/enigma.rb'
+
+class EnigmaTest < Minitest::Test
+
+  def test_it_exists
+    e = Enigma.new
+    assert_instance_of(Enigma, e)
+  end
+
+  def test_it_generates_key
+    e = Enigma.new
+    assert_equal 5, e.generate_key.length
+    assert_equal String, e.generate_key.class
+  end
+end
