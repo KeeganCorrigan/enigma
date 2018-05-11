5e0ef116146c6a99fa9e131aebe3e7e6fd2a62fe
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-05-11 16:44:03 -0600

    Rotate a-d tested.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -8,7 +8,28 @@ class Enigma
   def generate_key
     key = []
     5.times do
-    key.join << rand(0..9)
+    key << rand(0..9).to_s
     end
+    key
+  end
+
+  def rotate_a(key)
+    key[0] + key[1]
+  end
+
+  def rotate_b(key)
+    key[1] + key[2]
+  end
+
+  def rotate_c(key)
+    key[2] + key[3]
+  end
+
+  def rotate_d(key)
+    key[3] + key[4]
+  end
+
+  def time_used_for_offset
+    time = Time.now.to_i
   end
 end
