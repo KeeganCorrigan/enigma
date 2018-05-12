79788373154c2912f17f3ff364e4d0a9efaa63e5
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-05-12 00:11:51 -0600

    refactored rotater into one method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,38 +1,34 @@
 require 'pry'
 
 class Enigma
-  def initialize
 
+  attr_reader :key
+              :message_to_encrypt
+
+  def initialize(message_to_encrypt = "", key = generate_key)
+    @key = nil
   end
 
   def generate_key
-    key = []
-    5.times do
-    key << rand(0..9).to_s
+    if key == nil
+      key = ""
+      5.times do
+        key << rand(0..9).to_s
+      end
     end
-    key
+    return key
   end
 
-  def rotate_a(key)
-    # needs refactoring. It should do this all at once.
-    (key[0] + key[1]).split('').map do |number|
-      number.to_i
+  def rotater(key)
+    rotated_array = []
+    x = -1
+    4.times do
+      x += 1
+      rotated_array << (key[0 + x] + key[1 + x]).split('').map do |number|
+        number.to_i
+      end
     end
-  end
-
-  def rotate_b(key)
-    key[1] + key[2].split('').map do |number|
-      number.to_i
-  end
-
-  def rotate_c(key)
-    key[2] + key[3].split('').map do |number|
-      number.to_i
-  end
-
-  def rotate_d(key)
-    key[3] + key[4].split('').map do |number|
-      number.to_i
+    rotated_array
   end
 
   def time_used_for_offset
@@ -40,8 +36,28 @@ class Enigma
     offset = (time.to_i ** 2).to_s.split(//).last(4).join
     return offset.each_char.map(&:to_i)
   end
+end
+  # def rotate_b(key)
+  #   (key[1] + key[2]).split('').map do |number|
+  #     number.to_i
+  #   end
+  # end
+  #
+  # def rotate_c(key)
+  #   (key[2] + key[3]).split('').map do |number|
+  #     number.to_i
+  #   end
+  # end
+  #
+  # def rotate_d(key)
+  #   (key[3] + key[4]).split('').map do |number|
+  #     number.to_i
+  #   end
+  # end
+
+  # def convert_rotate_key_to_integer_array
+
 
   # def generate_shift_amount_for_a_rotation_and_offset
   #   rotate_a(key) + time_used_for_offset
   # end
-end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,5 +1,4 @@
 require 'pry'
-
 gem 'minitest', '~> 5.2'
 require 'minitest/autorun'
 require 'minitest/pride'
@@ -12,30 +11,21 @@ class EnigmaTest < Minitest::Test
     assert_instance_of(Enigma, e)
   end
 
-  def test_it_generates_key
+  def test_generates_key
     e = Enigma.new
     assert_equal 5, e.generate_key.length
-    assert_equal Array, e.generate_key.class
+    assert_equal String, e.generate_key.class
   end
 
-  def test_rotate_a_works_on_key
-    e = Enigma.new
-    assert_equal ["1","2"], e.rotate_a("12345")
+  def test_enigma_accepts_input_key
+    e = Enigma.new("12345")
+    assert_equal 5, e.generate_key.length
+    assert_equal String, e.generate_key.class
   end
 
-  def test_rotate_b_works_on_key
+  def test_rotate_works_on_a_key
     e = Enigma.new
-    assert_equal "23", e.rotate_b("12345")
-  end
-
-  def test_rotate_c_works_on_key
-    e = Enigma.new
-    assert_equal "34", e.rotate_c("12345")
-  end
-
-  def test_rotate_d_works_on_key
-    e = Enigma.new
-    assert_equal "45", e.rotate_d("12345")
+    assert_equal [[1,2],[2,3],[3, 4], [4, 5]], e.rotater("12345")
   end
 
   def test_generate_offset
@@ -47,9 +37,4 @@ class EnigmaTest < Minitest::Test
     assert_equal 4, e.time_used_for_offset.length
     assert_equal Array, e.time_used_for_offset.class
   end
-  #
-  # def test_rotate_offset_a
-  #   e = Enigma.new
-  #   assert_equal 21, e.rotate_a("12345").split('').to_i + e.time_used_for_offset
-  # end
 end
