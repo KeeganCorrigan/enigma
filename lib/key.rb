c69021bd7d12f7d9f5ad6d7b1215a1296b5c1505
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-05-16 16:46:12 -0600

    Rubocop to fix key.rb

--- lib/key.rb
+++ lib/key.rb
@@ -1,12 +1,11 @@
 require './lib/encryptor.rb'
-require 'pry'
 
 class Key
   def generate_key
     key = ''
     5.times do
-    key << ('0'..'9').to_a.sample
+      key << ('0'..'9').to_a.sample
     end
-    return key
+    key
   end
 end
