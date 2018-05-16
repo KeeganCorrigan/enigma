f9d59bfd8d54030bbfaa4fa78d906c4bb7e9bc3e
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-05-16 15:02:01 -0600

    Makes refacotors to  implentation code using rubocop specifications.

--- lib/crack.rb
+++ lib/crack.rb
@@ -4,8 +4,6 @@ encrypted = File.read(ARGV[0]).chomp
 
 e = Enigma.new
 
-
 File.write(ARGV[1], e.crack(encrypted, ARGV[2]))
 
 puts "Created 'cracked.txt' with the cracked key #{e.cracked_key} and the date #{ARGV[2]}"
-
--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,5 +1,5 @@
-require_relative 'encryptor.rb'
-require_relative 'cracker.rb'
+require './lib/encryptor.rb'
+require './lib/cracker.rb'
 
 class Enigma
   attr_reader :key,
@@ -20,7 +20,7 @@ class Enigma
     cipher_text
   end
 
-  def decrypt(my_message, key = nil , date = nil)
+  def decrypt(my_message, key = nil, date = nil)
     encryptor = Encryptor.new(my_message, key, date)
     plain_text = encryptor.decrypt
     @key = encryptor.key
@@ -30,13 +30,16 @@ class Enigma
 
   def crack(encrypted_message, date = nil)
     operator = :-
-    crack = Cracker.new(encrypted_message)
+    crack = Cracker.new(encrypted_message, date)
     cracked = crack.cracker(encrypted_message)
     @cracked_key = crack.brute_force_encryption_key(encrypted_message)
     encryptor = Encryptor.new
-    reversed_cipher_text = encrypted_message.reverse
-    reversed_message_index = encryptor.find_message_index_in_char_map(reversed_cipher_text)
-    cracked_message = encryptor.rotate_text_to_encrypt_and_decrypt(reversed_message_index, cracked.reverse, operator)
+    reversed_message_index = encryptor.find_message_index_in_char_map(encrypted_message.reverse)
+    cracked_message = encryptor.rotate_text_to_encrypt_and_decrypt(
+      reversed_message_index,
+      cracked.reverse,
+      operator
+    )
     cracked_message.reverse
   end
 end
