b0c1571d3515c5e13bb0cfadff5f4d33a142a741
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-05-16 15:19:16 -0600

    Rewrites tests to fulfill rubocop specifications.

--- test/cracker_test.rb
+++ test/cracker_test.rb
@@ -1,57 +1,54 @@
-require_relative 'test_helper'
+require './test/test_helper'
 require './lib/cracker.rb'
 require './lib/encryptor.rb'
 
-
 class CrackerTest < Minitest::Test
-
   def test_crack_exists
-    crack = Cracker.new("..end..", Date.new(2018, 5, 12))
+    crack = Cracker.new('..end..', Date.new(2018, 5, 12))
     assert_instance_of(Cracker, crack)
   end
 
   def test_crack_accepts_message
-    crack = Cracker.new("..end..", Date.new(2018, 5, 12))
-    assert_equal "..end..", crack.encrypted_message
+    crack = Cracker.new('..end..', Date.new(2018, 5, 12))
+    assert_equal '..end..', crack.encrypted_message
   end
 
   def test_crack_accepts_date
-    crack = Cracker.new("..end..", Date.new(2018, 5, 12))
-
+    crack = Cracker.new('..end..', Date.new(2018, 5, 12))
     assert_instance_of Date, crack.date
   end
 
   def test_crack_does_not_require_date
-    crack = Cracker.new("..end..")
+    crack = Cracker.new('..end..')
     assert_instance_of Date, crack.date
   end
 
   def test_cracker_finds_cipher_index
-    encrypted_message = "14iv8iyaais"
+    encrypted_message = '14iv8iyaais'
     crack = Cracker.new(encrypted_message, Date.new(2018, 5, 12))
     assert_equal [-13, -3, -29, -19], crack.cracker(encrypted_message)
   end
 
   def test_brute_force_encryption_key_returns_key
-    encrypted_message = "0sy07llnx1qnr"
+    encrypted_message = '0sy07llnx1qnr'
     crack = Cracker.new(encrypted_message)
-    assert_equal "11111", crack.brute_force_encryption_key(encrypted_message)
+    assert_equal '11111', crack.brute_force_encryption_key(encrypted_message)
   end
 
   def test_brute_force_encryption_key_returns_key_on_longer_string
     e = Enigma.new
-    message = "this is so secret ..end.."
-    output = e.encrypt("this is so secret ..end..", "75849")
+    message = 'this is so secret ..end..'
+    output = e.encrypt(message, '75849')
     crack = Cracker.new(output)
-    assert_equal "75849", crack.brute_force_encryption_key(output)
+    assert_equal '75849', crack.brute_force_encryption_key(output)
   end
 
   def test_brute_force_encryption_key_works_on_automatically_generated_key
     e = Enigma.new
-    message = "this is so secret ..end.."
-    output = e.encrypt("this is so secret ..end..")
+    message = 'this is so secret ..end..'
+    output = e.encrypt(message)
     crack = Cracker.new(output)
     decryption_key = crack.brute_force_encryption_key(output)
-    assert_equal "this is so secret ..end..", e.decrypt(output, decryption_key)
+    assert_equal 'this is so secret ..end..', e.decrypt(output, decryption_key)
   end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,14 +1,12 @@
-require_relative 'test_helper'
+require './test/test_helper'
 gem 'minitest', '~> 5.2'
 require './lib/encryptor.rb'
 require './lib/enigma.rb'
-require 'pry'
 
 class EnigmaTest < Minitest::Test
-
   def setup
     @e = Enigma.new
-    @secret = "this is so secret ..end.."
+    @secret = 'this is so secret ..end..'
     @encrypted = @e.encrypt(@secret)
   end
 
@@ -17,11 +15,10 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_encrypts_message
-    my_message = "hello"
-    actual = @e.encrypt(my_message, "12345", Date.new(2018, 5, 12))
-
-    assert_equal "14iv8", actual
-    refute_equal "14i99", actual
+    my_message = 'hello'
+    actual = @e.encrypt(my_message, '12345', Date.new(2018, 5, 12))
+    assert_equal '14iv8', actual
+    refute_equal '14i99', actual
   end
 
   def test_encrypts_message_without_key
@@ -29,16 +26,10 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_decrypts_short_encrypted_message
-    my_message = "14iv8"
-    actual = @e.decrypt(my_message, "12345", Date.new(2018, 5, 12))
-
-    assert_equal "hello", actual
-    refute_equal "hell0", actual
-  end
-
-  def test_decrypts_without_key
-    my_message = "lksjfdbnvklajsdnvlkajsbd"
-    actual = @e.decrypt(my_message)
+    my_message = '14iv8'
+    actual = @e.decrypt(my_message, '12345', Date.new(2018, 5, 12))
+    assert_equal 'hello', actual
+    refute_equal 'hell0', actual
   end
 
   def test_encrypt_works_on_short_message
@@ -56,33 +47,27 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_encrypts_and_then_decrypts_with_key_and_date
-    actual = @e.encrypt(@secret, "12345", Date.new(2018, 5, 12))
-
-    assert_equal "a7f2r8ph,b72y2ooax8iyaais", actual
-
-    expected = @e.decrypt(actual, "12345", Date.new(2018, 5, 12))
-
+    actual = @e.encrypt(@secret, '12345', Date.new(2018, 5, 12))
+    assert_equal 'a7f2r8ph,b72y2ooax8iyaais', actual
+    expected = @e.decrypt(actual, '12345', Date.new(2018, 5, 12))
     assert_equal @secret, expected
   end
 
   def test_crack_works_on_short_encrypted_message
-    encrypted_message = "14iv8x8iyaais"
-
+    encrypted_message = '14iv8x8iyaais'
     actual = @e.crack(encrypted_message)
-    assert "hello ..end..", actual
+    assert 'hello ..end..', actual
   end
 
   def test_crack_works_on_longer_encrypted_message
-    encrypted_message = "a7f2r8ph,b72y2ooax8iyaais"
+    encrypted_message = 'a7f2r8ph,b72y2ooax8iyaais'
     actual = @e.crack(encrypted_message)
-
     assert @secret, actual
   end
 
   def test_crack_works_on_different_words
-    my_message = "this is a test ..end.."
-
-    output = @e.encrypt(my_message, "12345", Date.today)
+    my_message = 'this is a test ..end..'
+    output = @e.encrypt(my_message, '12345', Date.today)
     assert my_message, @e.crack(output)
   end
 end
